34f8400056ed724fe8cad57d0de7e28b1435263f
amril <amrilsyaifa@gmail.com>
2020-04-11 20:30:59 +0700

    calender

--- App.js
+++ App.js
@@ -20,11 +20,14 @@ import { Notifications } from 'react-native-notifications';
 
 import {
   Header,
-  LearnMoreLinks,
   Colors,
-  DebugInstructions,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+
+const vacation = { key: 'vacation', color: 'red', selectedDotColor: 'blue' };
+const massage = { key: 'massage', color: 'blue', selectedDotColor: 'blue' };
+const workout = { key: 'workout', color: 'green' };
 
 class App extends Component {
   constructor(props) {
@@ -69,6 +72,8 @@ class App extends Component {
     });
   }
   render() {
+
+    //https://github.com/wix/react-native-calendars
     return (
       <>
         <StatusBar barStyle="dark-content" />
@@ -90,7 +95,114 @@ class App extends Component {
                 accessibilityLabel="Learn more about this purple button"
               />
             </View>
-
+            <View>
+              <Calendar
+                // Initially visible month. Default = Date()
+                current={'2020-04-11'}
+                // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+                minDate={'2012-05-10'}
+                // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+                maxDate={'2012-05-30'}
+                // Handler which gets executed on day press. Default = undefined
+                onDayPress={(day) => { console.log('selected day', day) }}
+                // Handler which gets executed on day long press. Default = undefined
+                onDayLongPress={(day) => { console.log('selected day', day) }}
+                // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+                monthFormat={'yyyy MM'}
+                // Handler which gets executed when visible month changes in calendar. Default = undefined
+                onMonthChange={(month) => { console.log('month changed', month) }}
+                // Hide month navigation arrows. Default = false
+                hideArrows={true}
+                // Replace default arrows with custom ones (direction can be 'left' or 'right')
+                renderArrow={(direction) => (<Arrow />)}
+                // Do not show days of other months in month page. Default = false
+                hideExtraDays={true}
+                // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
+                // day from another month that is visible in calendar page. Default = false
+                disableMonthChange={true}
+                // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+                firstDay={1}
+                // Hide day names. Default = false
+                hideDayNames={true}
+                // Show week numbers to the left. Default = false
+                showWeekNumbers={true}
+                // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+                onPressArrowLeft={substractMonth => substractMonth()}
+                // Handler which gets executed when press arrow icon right. It receive a callback can go next month
+                onPressArrowRight={addMonth => addMonth()}
+                // Disable left arrow. Default = false
+                disableArrowLeft={true}
+                // Disable right arrow. Default = false
+                disableArrowRight={true}
+                markedDates={{
+                  '2020-04-11': { selected: true, marked: true, selectedColor: 'blue' },
+                  '2020-04-12': { marked: true },
+                  '2020-04-13': { marked: true, dotColor: 'red', activeOpacity: 0 },
+                  '2020-04-14': { disabled: true, disableTouchEvent: true },
+                  '2020-04-15': { dots: [vacation, massage, workout], selected: true, selectedColor: 'red' },
+                  '2020-04-16': { dots: [massage, workout], disabled: true },
+                  '2020-04-17': { textColor: 'green' },
+                  '2020-04-20': { startingDay: true, color: 'green' },
+                  '2020-04-21': { selected: true, endingDay: true, color: 'green', textColor: 'gray' },
+                  '2020-04-04': { disabled: true, startingDay: true, color: 'green', endingDay: true }
+                }}
+                markingType={'period'}
+              // markingType={'multi-dot'}
+              />
+              <Calendar
+                // Initially visible month. Default = Date()
+                current={'2020-04-11'}
+                // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+                minDate={'2012-05-10'}
+                // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+                maxDate={'2012-05-30'}
+                // Handler which gets executed on day press. Default = undefined
+                onDayPress={(day) => { console.log('selected day', day) }}
+                // Handler which gets executed on day long press. Default = undefined
+                onDayLongPress={(day) => { console.log('selected day', day) }}
+                // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+                monthFormat={'yyyy MM'}
+                // Handler which gets executed when visible month changes in calendar. Default = undefined
+                onMonthChange={(month) => { console.log('month changed', month) }}
+                // Hide month navigation arrows. Default = false
+                hideArrows={true}
+                // Replace default arrows with custom ones (direction can be 'left' or 'right')
+                renderArrow={(direction) => (<Arrow />)}
+                // Do not show days of other months in month page. Default = false
+                hideExtraDays={true}
+                // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
+                // day from another month that is visible in calendar page. Default = false
+                disableMonthChange={true}
+                // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+                firstDay={1}
+                // Hide day names. Default = false
+                hideDayNames={true}
+                // Show week numbers to the left. Default = false
+                showWeekNumbers={true}
+                // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+                onPressArrowLeft={substractMonth => substractMonth()}
+                // Handler which gets executed when press arrow icon right. It receive a callback can go next month
+                onPressArrowRight={addMonth => addMonth()}
+                // Disable left arrow. Default = false
+                disableArrowLeft={true}
+                // Disable right arrow. Default = false
+                disableArrowRight={true}
+                markedDates={{
+                  '2020-04-11': { selected: true, marked: true, selectedColor: 'blue' },
+                  '2020-04-12': { marked: true },
+                  '2020-04-13': { marked: true, dotColor: 'red', activeOpacity: 0 },
+                  '2020-04-14': { disabled: true, disableTouchEvent: true },
+                  '2020-04-15': { dots: [vacation, massage, workout], selected: true, selectedColor: 'red' },
+                  '2020-04-16': { dots: [massage, workout], disabled: true },
+                  '2020-04-17': { textColor: 'green' },
+                  '2020-04-20': { startingDay: true, color: 'green' },
+                  '2020-04-21': { selected: true, endingDay: true, color: 'green', textColor: 'gray' },
+                  '2020-04-04': { disabled: true, startingDay: true, color: 'green', endingDay: true }
+                }}
+                // markingType={'period'}
+                markingType={'multi-dot'}
+              />
+            </View>
           </ScrollView>
         </SafeAreaView>
       </>
